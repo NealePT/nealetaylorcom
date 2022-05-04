eb0208c319a39cbbde3ad9aaaaf4ecac33cbb3b7
Neale Taylor <nealeptaylor@gmail.com>
2022-05-04 16:54:04 -0400

    add logic for linkbar transtition

--- react-site/src/components/LinkBar.jsx
+++ react-site/src/components/LinkBar.jsx
@@ -1,25 +1,37 @@
 export default function LinkBar(props) {
+  const handleClick = () => {
+    return (
+      <span>
+      {props.linkClick 
+        ?
+        <div className="slide-out">
+        <nav className="topnav">
+          <ul className="linkButtons">
+            <li onClick={(e) => {
+              props.setLinkClick(false)
+            }}><a className="clickOff"><i className="fa-solid fa-angles-right"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/nealeptaylor/"><i className="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="https://github.com/NealePT"><i className="fa-brands fa-github-square"></i></a></li>
+            <li><a href="https://www.instagram.com/nealeptaylor/"><i className="fa-brands fa-instagram-square"></i></a></li>
+            <li><a href="mailto:nealeptaylor@gmail.com"><i className="fa-solid fa-square-envelope"></i></a></li>
+          </ul>
+        </nav>
+        </div> 
+        : 
+        <div className="arrowButton" onClick={(e) => {
+          props.setLinkClick(true)
+        }}>
+          <a className="click">
+            <i className="fa-solid fa-angles-left"></i>
+          </a>
+        </div>
+      }
+      </span>
+    )
+  }
+
+
   return (
-    <span>
-      <div className="slide-out">
-      <nav className="topnav">
-        <ul className="linkButtons">
-          <li onClick={(e) => {
-            props.setLinkClick(true)
-            console.log(props.linkClick)
-          }}><a className="clickOff"><i className="fa-solid fa-angles-right"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/nealeptaylor/"><i className="fa-brands fa-linkedin"></i></a></li>
-          <li><a href="https://github.com/NealePT"><i className="fa-brands fa-github-square"></i></a></li>
-          <li><a href="https://www.instagram.com/nealeptaylor/"><i className="fa-brands fa-instagram-square"></i></a></li>
-          <li><a href="mailto:nealeptaylor@gmail.com"><i className="fa-solid fa-square-envelope"></i></a></li>
-        </ul>
-      </nav>
-      </div>
-      <div className="arrowButton">
-        <a className="click">
-          <i className="fa-solid fa-angles-left"></i>
-        </a>
-      </div>
-    </span>
+    handleClick()
   )
 }
