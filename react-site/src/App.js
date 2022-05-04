e9657f22466e28e821d6db272fa25c5c2183e946
Neale Taylor <nealeptaylor@gmail.com>
2022-05-04 16:53:45 -0400

    remove unnecessary code

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import { useState } from 'react';
 
 function App() {
-  const [linkClick, setLinkClick] = useState(false);
+  const [linkClick, setLinkClick] = useState(true);
 
   return (
     <div className="App">
