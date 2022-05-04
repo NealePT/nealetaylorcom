7326f0f5dbe50d9886dccbe080c1bc4d20733385
Neale Taylor <nealeptaylor@gmail.com>
2022-05-04 15:32:01 -0400

    add link bar

--- react-site/src/components/Header.jsx
+++ react-site/src/components/Header.jsx
@@ -1,23 +1,10 @@
+import LinkBar from "./LinkBar"
+
 export default function Header(props) {
   return (
     <header>
+    <LinkBar />
     <div className="wrapper">
-      <div className="slide-out">
-        <nav className="topnav">
-          <ul className="linkButtons">
-            <li><a className="clickOff"><i className="fa-solid fa-angles-right"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/nealeptaylor/"><i className="fa-brands fa-linkedin"></i></a></li>
-            <li><a href="https://github.com/NealePT"><i className="fa-brands fa-github-square"></i></a></li>
-            <li><a href="https://www.instagram.com/nealeptaylor/"><i className="fa-brands fa-instagram-square"></i></a></li>
-            <li><a href="mailto:nealeptaylor@gmail.com"><i className="fa-solid fa-square-envelope"></i></a></li>
-          </ul>
-        </nav>
-      </div>
-      <div className="arrowButton">
-        <a className="click">
-          <i className="fa-solid fa-angles-left"></i>
-        </a>
-      </div>
       <div className="titleCard">
         <h1>
           Neale Taylor
