64f504b20444135f04e3982fb4e7e1a196dfe690
Neale Taylor <nealeptaylor@gmail.com>
2022-05-09 14:55:26 -0400

    add components to about section

--- react-site/src/components/About.jsx
+++ react-site/src/components/About.jsx
@@ -2,8 +2,21 @@ export default function About(props) {
   return (
     <div className="wrapper">
       <div className="about">
-        <div className="profilepic">
-          <img src="../../public/assets/CC52E52D-F3E0-429D-ABC9-07B8A150D0B0.jpg" alt="Profile Pic" />
+        <h3>
+          About Me
+        </h3>
+        <div className="aboutFlex">
+          <div className="profilepic">
+            <img src="../../assets/profilePic.jpg" alt="Profile Pic" />
+          </div>
+          <div className="aboutDescription">
+            <p>
+              Web Developer adept in front-end and back-end solutions. Proficient in an assortment
+    of technologies including Javascript, React, HTML/CSS, SQL and more. Able to
+    effectively self-manage during independent work as well as collaborate on projects and
+    lead teams to complete tasks.
+            </p>
+          </div>
         </div>
       </div>
     </div>
