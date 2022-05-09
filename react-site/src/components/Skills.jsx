aa1bf72b7e45bfba60b27af317b892ba5ecd88ed
Neale Taylor <nealeptaylor@gmail.com>
2022-05-09 16:36:59 -0400

    modify css

--- react-site/src/components/Skills.jsx
+++ react-site/src/components/Skills.jsx
@@ -1,9 +1,14 @@
 export default function Skills(props) {
   return (
     <div className="skills">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam suscipit debitis laboriosam totam fuga provident voluptate voluptatibus atque eaque, esse repellat modi ad vitae, inventore ab, ipsa magni voluptas aliquam?
-      Rem ex deserunt veniam ducimus laboriosam asperiores dicta aut temporibus obcaecati eligendi non debitis rerum fugit quae, quam atque at delectus doloremque architecto harum, necessitatibus quos. Non similique voluptatem quaerat.
-      Vero ex sed ea voluptas similique mollitia rem exercitationem aut quis itaque nam quaerat, a assumenda ipsum autem commodi. In quo recusandae consectetur nesciunt! Illo pariatur alias temporibus optio vero.
+      <div className="wrapper">
+        <h3>
+          Skills
+        </h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam suscipit debitis laboriosam totam fuga provident voluptate voluptatibus atque eaque, esse repellat modi ad vitae, inventore ab, ipsa magni voluptas aliquam?
+        Rem ex deserunt veniam ducimus laboriosam asperiores dicta aut temporibus obcaecati eligendi non debitis rerum fugit quae, quam atque at delectus doloremque architecto harum, necessitatibus quos. Non similique voluptatem quaerat.
+        Vero ex sed ea voluptas similique mollitia rem exercitationem aut quis itaque nam quaerat, a assumenda ipsum autem commodi. In quo recusandae consectetur nesciunt! Illo pariatur alias temporibus optio vero.
+      </div>
     </div>
   )
 }
