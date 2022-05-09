14fca5014042bd761881577c52afb23afa648543
Neale Taylor <nealeptaylor@gmail.com>
2022-05-09 18:18:10 -0400

    import skills card component

--- react-site/src/components/Skills.jsx
+++ react-site/src/components/Skills.jsx
@@ -1,3 +1,5 @@
+import SkillsCard from "./SkillsCard"
+
 export default function Skills(props) {
   return (
     <div className="skills">
@@ -5,6 +7,7 @@ export default function Skills(props) {
         <h3>
           Skills
         </h3>
+        <SkillsCard />
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam suscipit debitis laboriosam totam fuga provident voluptate voluptatibus atque eaque, esse repellat modi ad vitae, inventore ab, ipsa magni voluptas aliquam?
         Rem ex deserunt veniam ducimus laboriosam asperiores dicta aut temporibus obcaecati eligendi non debitis rerum fugit quae, quam atque at delectus doloremque architecto harum, necessitatibus quos. Non similique voluptatem quaerat.
         Vero ex sed ea voluptas similique mollitia rem exercitationem aut quis itaque nam quaerat, a assumenda ipsum autem commodi. In quo recusandae consectetur nesciunt! Illo pariatur alias temporibus optio vero.
