eee8e88193db2b515c6b0461189ddc5ed7d3e0da
Neale Taylor <nealeptaylor@gmail.com>
2022-05-09 14:37:46 -0400

    add test code

--- react-site/src/components/About.jsx
+++ react-site/src/components/About.jsx
@@ -1,7 +1,11 @@
 export default function About(props) {
   return (
     <div className="wrapper">
-      
+      <div className="about">
+        <div className="profilepic">
+          <img src="../../public/assets/CC52E52D-F3E0-429D-ABC9-07B8A150D0B0.jpg" alt="Profile Pic" />
+        </div>
+      </div>
     </div>
   )
 }
