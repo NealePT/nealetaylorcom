afd8d467fde095264496dd616754bde5c4199479
Neale Taylor <nealeptaylor@gmail.com>
2022-05-10 16:59:50 -0400

    add front end skills section

--- react-site/src/components/SkillsCard.jsx
+++ react-site/src/components/SkillsCard.jsx
@@ -1,7 +1,21 @@
 export default function SkillsCard(props) {
   return (
     <div className="skillcard">
-      
+      <div className="frontEndSkills">
+        <h5>
+          Front-End
+        </h5>
+        <ul>
+          <li>JavaScript</li>
+          <li>Ruby</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>React</li>
+          <li>BootStrap</li>
+          <li>Sass/SCSS</li>
+          <li>Storybook</li>
+        </ul>
+      </div>
     </div>
   )
 }
