bd634601f94826265cb2fe7e5817638a53065e5b
Neale Taylor <nealeptaylor@gmail.com>
2022-05-11 15:51:57 -0400

    modify skills

--- react-site/src/components/Skills.jsx
+++ react-site/src/components/Skills.jsx
@@ -8,9 +8,6 @@ export default function Skills(props) {
           Skills
         </h3>
         <SkillsCard />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam suscipit debitis laboriosam totam fuga provident voluptate voluptatibus atque eaque, esse repellat modi ad vitae, inventore ab, ipsa magni voluptas aliquam?
-        Rem ex deserunt veniam ducimus laboriosam asperiores dicta aut temporibus obcaecati eligendi non debitis rerum fugit quae, quam atque at delectus doloremque architecto harum, necessitatibus quos. Non similique voluptatem quaerat.
-        Vero ex sed ea voluptas similique mollitia rem exercitationem aut quis itaque nam quaerat, a assumenda ipsum autem commodi. In quo recusandae consectetur nesciunt! Illo pariatur alias temporibus optio vero.
       </div>
     </div>
   )
