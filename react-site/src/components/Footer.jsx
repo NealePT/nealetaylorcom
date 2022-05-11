bef9e4d435e52fd291f830386993c6b9c939eb77
Neale Taylor <nealeptaylor@gmail.com>
2022-05-11 15:58:19 -0400

    add content to footer

--- react-site/src/components/Footer.jsx
+++ react-site/src/components/Footer.jsx
@@ -2,7 +2,15 @@ export default function Footer(props) {
   return (
     <div className="footer">
       <div className="wrapper">
-        
+        <nav class="footnav">
+          <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#skills">Skills</a></li>
+              <li><a href="#projects">Projects</a></li>
+          </ul>
+          <p>Created at Juno College of Technology</p>
+        </nav>
       </div>
     </div>
   )
