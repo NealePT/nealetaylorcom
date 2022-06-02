d98fe535f71dcbc9e5bde215dbfce100342744ee
Neale Taylor <nealeptaylor@gmail.com>
2022-06-02 18:16:47 -0400

    separate sides

--- react-site/src/components/Skills.jsx
+++ react-site/src/components/Skills.jsx
@@ -7,31 +7,33 @@ export default function Skills(props) {
         <h3>
           Skills
         </h3>
-        <aside className="skillSidebar">
-          <h5 onClick={(e) => {
-            props.setSkill("FrontEnd")
-          }}>Front-End</h5>
-          <h5 onClick={(e) => {
-            props.setSkill("BackEnd")
-          }}>Back-End</h5>
-          <h5 onClick={(e) => {
-            props.setSkill("Testing")
-          }}>Testing</h5>
-        </aside>
-        <span>
-        {props.skill === "FrontEnd" && 
-        <div>
-          hi
-        </div>}
-        {props.skill === "BackEnd" && 
-        <div>
-          sup
-        </div>}
-        {props.skill === "Testing" && 
-        <div>
-          yo
-        </div>}
-      </span>
+        <div className="skillContainer">
+          <aside className="skillSidebar">
+            <h5 onClick={(e) => {
+              props.setSkill("FrontEnd")
+            }}>Front-End</h5>
+            <h5 onClick={(e) => {
+              props.setSkill("BackEnd")
+            }}>Back-End</h5>
+            <h5 onClick={(e) => {
+              props.setSkill("Testing")
+            }}>Testing</h5>
+          </aside>
+          <span>
+            {props.skill === "FrontEnd" && 
+            <div>
+              hi
+            </div>}
+            {props.skill === "BackEnd" && 
+            <div>
+              sup
+            </div>}
+            {props.skill === "Testing" && 
+            <div>
+              yo
+            </div>}
+          </span>
+        </div>
         {/* <SkillsCard /> */}
       </div>
     </div>
