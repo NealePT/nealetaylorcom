a3e8f30b19de626720125319782443aa6f6b8f21
Neale Taylor <nealeptaylor@gmail.com>
2022-06-08 15:41:01 -0400

    add cards

--- react-site/src/components/Education.jsx
+++ react-site/src/components/Education.jsx
@@ -2,8 +2,29 @@ import EducationCard from "./EducationCard"
 
 export default function Education(props) {
   return (
-    <div>
-      
+    <div id="education" className="education">
+      <div className="wrapper">
+        <h3>
+          Education
+        </h3>
+        <div className="aboutCards">
+        <EducationCard
+          type={"Education"}
+          icon={<i className="fa-solid fa-graduation-cap"></i>}
+          description={"Lighthouse Labs"}
+        />
+        <EducationCard
+          type={"Experience"}
+          icon={<i className="fa-solid fa-briefcase"></i>}
+          description={"1 Year"}
+        />
+        <EducationCard
+          type={"Projects"}
+          icon={<i className="fa-solid fa-folder-closed"></i>}
+          description={"Github"}
+        />
+      </div>
+      </div>
     </div>
   )
 }
