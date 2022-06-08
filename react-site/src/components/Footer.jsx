25afb23a9769d838e028cbdce23bf33288f38856
Neale Taylor <nealeptaylor@gmail.com>
2022-06-08 16:41:51 -0400

    add education tab to footer

--- react-site/src/components/Footer.jsx
+++ react-site/src/components/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer(props) {
           <ul>
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
+              <li><a href="#education">Education</a></li>
               <li><a href="#skills">Skills</a></li>
               <li><a href="#projects">Projects</a></li>
           </ul>
