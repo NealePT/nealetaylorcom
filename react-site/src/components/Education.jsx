44bf1bd4dd8fce282fa748b9b43353953b110dee
Neale Taylor <nealeptaylor@gmail.com>
2022-06-08 16:48:32 -0400

    change education logos

--- react-site/src/components/Education.jsx
+++ react-site/src/components/Education.jsx
@@ -10,12 +10,12 @@ export default function Education(props) {
         <div className="aboutCards">
         <EducationCard
           type={"Queen's University"}
-          icon={<img src="../../assets/QueensLogo_colour.png" alt="Queen's University Logo" />}
+          icon={<img src="../../assets/queenslogowhite.jpg" alt="Queen's University Logo" />}
           description={"Bachelor of Arts (Honours)"}
         />
         <EducationCard
           type={"Lighthouse Labs"}
-          icon={<img className="lighthouselogo" src="../../assets/lighthouseTransparentLogo.png" alt="Lighthouse Labs Logo" />}
+          icon={<img className="lighthouselogo" src="../../assets/lighthouseLogo.jpeg" alt="Lighthouse Labs Logo" />}
           description={"Diploma- Full-Stack Web Development"}
         />
       </div>
