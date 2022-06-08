8a25041b30cae5bf33708ac5f529fc3983cf59f2
Neale Taylor <nealeptaylor@gmail.com>
2022-06-08 15:20:32 -0400

    remove unnecessary code

--- react-site/src/components/About.jsx
+++ react-site/src/components/About.jsx
@@ -12,23 +12,6 @@ export default function About(props) {
             <img src="../../assets/profilePic.jpg" alt="Profile Pic" />
           </div>
           <div className="aboutDescription">
-            {/* <div className="aboutCards">
-              <AboutCard 
-                type={"Education"}
-                icon={<i className="fa-solid fa-graduation-cap"></i>}
-                description={"Lighthouse Labs"}
-              />
-              <AboutCard 
-                type={"Experience"}
-                icon={<i className="fa-solid fa-briefcase"></i>}
-                description={"1 Year"}
-              />
-              <AboutCard 
-                type={"Projects"}
-                icon={<i className="fa-solid fa-folder-closed"></i>}
-                description={"Github"}
-              />
-            </div> */}
             <p className="aboutme">
             I am a Web Developer adept in front-end and back-end solutions. I've worked with and have become proficient in an assortment of technologies including Javascript, React, HTML/CSS, SQL and more. I am also able to effectively self-manage during independent work as well as collaborate on projects and lead teams to complete tasks.
             </p>
