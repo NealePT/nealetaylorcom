fc03dd9c3b54f0e83861c257c0ed9d2cacb90cbd
Neale Taylor <nealeptaylor@gmail.com>
2022-06-08 17:01:52 -0400

    add projects header

--- react-site/src/components/Projects.jsx
+++ react-site/src/components/Projects.jsx
@@ -2,7 +2,9 @@ export default function Projects(props) {
   return (
     <div id="projects" className="projects">
       <div className="wrapper">
-        
+        <h3>
+          Projects
+        </h3>
       </div>
     </div>
   )
