95d322bfada5dd0dd57e21fc6bd94c57bd261934
Neale Taylor <nealeptaylor@gmail.com>
2022-06-09 17:01:52 -0400

    add content to projects

--- react-site/src/components/Projects.jsx
+++ react-site/src/components/Projects.jsx
@@ -19,18 +19,22 @@ export default function Projects(props) {
         <div class="item">
           <img src="../../assets/schedulerScreenshot.png" alt="Scheduler Screenshot"  />
           <h5>Interview Scheduler</h5>
+          <h6>Interview Scheduler is a Single Page Application built for creating and tracking student interviews.</h6>
         </div>
         <div class="item">
           <img src="../../assets/jungleScreenshot.png" alt="Jungle Screenshot"  />
           <h5>Jungle</h5>
+          <h6>A mini e-commerce application built with Rails 4.2 for purposes of learning Rails.</h6>
         </div>
         <div class="item">
           <img src="../../assets/tweeterScreenshot.png" alt="Tweeter Screenshot"  />
           <h5>Tweeter</h5>
+          <h6>Single-page Twitter clone where user can post and interact with tweets.</h6>
         </div>
         <div class="item">
           <img src="../../assets/tinyUrlScreenshot.png" alt="TinyURL Screenshot"  />
           <h5>TinyURL</h5>
+          <h6>TinyApp is a full stack web application that allows users to shorten long URLs (à la bit.ly).</h6>
         </div>
         </div>
       </div>
