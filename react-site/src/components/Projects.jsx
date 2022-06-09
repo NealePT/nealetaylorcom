59f3c4cf152e3cefe14d0aea80271af8a5327bd1
Neale Taylor <nealeptaylor@gmail.com>
2022-06-09 16:53:28 -0400

    add info to div items

--- react-site/src/components/Projects.jsx
+++ react-site/src/components/Projects.jsx
@@ -6,42 +6,30 @@ export default function Projects(props) {
           Projects
         </h3>
         <div class="carouselWrap">
-          <section id="section1">
-            <a href="#section3" class="arrow__btn">‹</a>
-            <div class="item">
-            <img src="../../assets/simpliScreenshot.png" alt="Simpli Screenshot"  />
-          </div>
-            <div class="item">
-            <img src="../../assets/resourceHavenScreenshot.png" alt="Resource Haven Screenshot"  />
-          </div>
-            <a href="#section2" class="arrow__btn">›</a>
-          </section>
-
-
-
-          <section id="section2">
-            <a href="#section1" class="arrow__btn">‹</a>
-            <div class="item">
-            <img src="../../assets/schedulerScreenshot.png" alt="Scheduler Screenshot"  />
-          </div>
-            <div class="item">
-            <img src="../../assets/jungleScreenshot.png" alt="Jungle Screenshot"  />
-          </div>
-
-
-
-            <a href="#section3" class="arrow__btn">›</a>
-          </section>
-          <section id="section3">
-            <a href="#section2" class="arrow__btn">‹</a>
-            <div class="item">
-            <img src="../../assets/tweeterScreenshot.png" alt="Tweeter Screenshot"  />
-          </div>
-            <div class="item">
-            <img src="../../assets/tinyUrlScreenshot.png" alt="TinyURL Screenshot"  />
-          </div>
-            <a href="#section1" class="arrow__btn">›</a>
-          </section>
+        <div class="item">
+          <img src="../../assets/simpliScreenshot.png" alt="Simpli Screenshot"  />
+          <h5>Simpli</h5>
+        </div>
+        <div class="item">
+          <img src="../../assets/resourceHavenScreenshot.png" alt="Resource Haven Screenshot"  />
+          <h5>Resource Haven</h5>
+        </div>
+        <div class="item">
+          <img src="../../assets/schedulerScreenshot.png" alt="Scheduler Screenshot"  />
+          <h5>Interview Scheduler</h5>
+        </div>
+        <div class="item">
+          <img src="../../assets/jungleScreenshot.png" alt="Jungle Screenshot"  />
+          <h5>Jungle</h5>
+        </div>
+        <div class="item">
+          <img src="../../assets/tweeterScreenshot.png" alt="Tweeter Screenshot"  />
+          <h5>Tweeter</h5>
+        </div>
+        <div class="item">
+          <img src="../../assets/tinyUrlScreenshot.png" alt="TinyURL Screenshot"  />
+          <h5>TinyURL</h5>
+        </div>
         </div>
       </div>
     </div>
