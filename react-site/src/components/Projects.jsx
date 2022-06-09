8c06e82307637cb8b17ba7c6579010ef35b72442
Neale Taylor <nealeptaylor@gmail.com>
2022-06-09 15:33:26 -0400

    all screenshots added

--- react-site/src/components/Projects.jsx
+++ react-site/src/components/Projects.jsx
@@ -22,10 +22,10 @@ export default function Projects(props) {
           <section id="section2">
             <a href="#section1" class="arrow__btn">‹</a>
             <div class="item">
-            <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABVopDZ5Fy9_fk_HO5WxHTXKKjKhtWFupbSjuvPwfLK_vytzon4EwRUdGgYJ34JwPxOTK_NkV3aXfkULMB0Dcct-FyfqzH-X44VXuRMp4QeBHlvKwWeZFpZlGdItPzmmg4scmwhG7SQ.jpg" alt="Describe " />
+            <img src="../../assets/schedulerScreenshot.png" alt="Scheduler Screenshot"  />
           </div>
             <div class="item">
-            <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTOj1-116yVcgKWMU2dI3GFR4x0fSkiGsqtLLeLUxRR7STaksjAqBTrYlTfrB8nIGnGvXksi0ewXAhVGg6-pLxpFOIfcpjK-pf8D5xehFZo5a6vJbo4L0AGbrzglbyUoq255QBJgRQ.jpg" alt="Describe " />
+            <img src="../../assets/jungleScreenshot.png" alt="Jungle Screenshot"  />
           </div>
 
 
@@ -35,13 +35,10 @@ export default function Projects(props) {
           <section id="section3">
             <a href="#section2" class="arrow__btn">‹</a>
             <div class="item">
-            <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRr4YxdaABuAuH_3FmSQZn7BCvLp-UUPsXE9MiYpvFP3CSUHV2zOew5oVqKqqdaOd3tbFVS0Uf67uIs7_eZydlCghg4nK0nMatRpPImABwTOhnNzCLCxdKrua7pPIcPCZqBYTeAO5g.jpg" alt="Describe " />
+            <img src="../../assets/tweeterScreenshot.png" alt="Tweeter Screenshot"  />
           </div>
             <div class="item">
-            <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTyWK1MKaw8GcObtz47R2Tj7wkLJ7qQu9tk6TVpcoyxpzD4B-zZ569bQ5vGrREBL-MWFkGilXUwy7tCDaj2XOGkUB4RsbbFAmp9NgSr6lygMpUGNHSlyfrFbUORsRkrxSIoh_ggOvg.jpg" alt="Describe " />
-          </div>
-            <div class="item">
-            <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABY7NwkWEJIfXsn6t3Li4bGSCQ1nEErPisI5ZZtXlC-_VRBZOUrhWK5X3vt3t6SR_cpgVhCwxgQqFFDJhk62Kk8hawOnYGZMr0LKeLczMFV2zalCFjkcdLksvT4HB2LEi6LFyruyk3Uu0LmNGsHfC2A8Bly60smr_3sDbz4RruXcklPOG1qYq9wUVu3zfaiwNvqmG4b8aFA.jpg" alt="Describe " />
+            <img src="../../assets/tinyUrlScreenshot.png" alt="TinyURL Screenshot"  />
           </div>
             <a href="#section1" class="arrow__btn">›</a>
           </section>
