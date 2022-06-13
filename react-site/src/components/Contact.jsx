1db1cf8b5f0a46a0c63b9cd274abb7db814225aa
Neale Taylor <nealeptaylor@gmail.com>
2022-06-13 16:42:03 -0400

    add h3

--- react-site/src/components/Contact.jsx
+++ react-site/src/components/Contact.jsx
@@ -1,7 +1,11 @@
 export default function Contact(props) {
   return (
     <div className="contact">
-      
+      <div className="wrapper">
+        <h3>
+          Contact Me
+        </h3>
+      </div>
     </div>
   )
 }
