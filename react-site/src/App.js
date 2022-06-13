8b1122651734baa9b4adf328913c6a6ed75ee6db
Neale Taylor <nealeptaylor@gmail.com>
2022-06-13 16:42:34 -0400

    add contact

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Education from './components/Education';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { useState } from 'react';
 import './App.css';
