f93c7e2499c91f6b67fa481b5ea0fb85b4670a74
Neale Taylor <nealeptaylor@gmail.com>
2022-06-14 13:47:54 -0400

    add email link

--- react-site/src/components/Contact.jsx
+++ react-site/src/components/Contact.jsx
@@ -5,6 +5,12 @@ export default function Contact(props) {
         <h3>
           Contact Me
         </h3>
+        <div className="conactLinks">
+          <a className="contactEmailLink" href="mailto:nealeptaylor@gmail.com">
+            <i className="fa-solid fa-square-envelope"></i>
+            <p>nealeptaylor@gmail.com</p>
+          </a>
+        </div>
       </div>
     </div>
   )
