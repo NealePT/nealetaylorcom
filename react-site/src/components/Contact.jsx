94bc4d0d7161f325a074d360c8922b2d1784084a
Neale Taylor <nealeptaylor@gmail.com>
2022-06-14 14:23:41 -0400

    change div name

--- react-site/src/components/Contact.jsx
+++ react-site/src/components/Contact.jsx
@@ -5,10 +5,14 @@ export default function Contact(props) {
         <h3>
           Contact Me
         </h3>
-        <div className="conactLinks">
-          <a className="contactLink contactEmailLink" href="mailto:nealeptaylor@gmail.com">
+        <div className="contactLinks">
+          <a className="contactLink" href="mailto:nealeptaylor@gmail.com">
             <i class="fa-solid fa-envelope"></i>
             <h5>Email</h5>
+          </a>
+          <a className="contactLink" href="https://www.linkedin.com/in/nealeptaylor/">
+            <i className="fa-brands fa-linkedin"></i>
+            <h5>LinkedIn</h5>
           </a>
         </div>
       </div>
