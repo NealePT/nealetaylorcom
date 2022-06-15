368b6bce76538ae1409cc7db1ac976f1e472035f
Neale Taylor <nealeptaylor@gmail.com>
2022-06-15 15:05:48 -0400

    reorder links on sidebar

--- react-site/src/components/LinkBar.jsx
+++ react-site/src/components/LinkBar.jsx
@@ -10,10 +10,10 @@ export default function LinkBar(props) {
             <li onClick={(e) => {
               props.setLinkClick(false)
             }}><a className="clickOff"><i className="fa-solid fa-angles-right"></i></a></li>
+            <li><a href="mailto:nealeptaylor@gmail.com"><i className="fa-solid fa-square-envelope"></i></a></li>
             <li><a href="https://www.linkedin.com/in/nealeptaylor/"><i className="fa-brands fa-linkedin"></i></a></li>
             <li><a href="https://github.com/NealePT"><i className="fa-brands fa-github-square"></i></a></li>
             <li><a href="https://www.instagram.com/nealeptaylor/"><i className="fa-brands fa-instagram-square"></i></a></li>
-            <li><a href="mailto:nealeptaylor@gmail.com"><i className="fa-solid fa-square-envelope"></i></a></li>
           </ul>
         </nav>
         </div> 
