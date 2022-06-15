64603a7fb75f15f4d8edfba133c4602b454cb425
Neale Taylor <nealeptaylor@gmail.com>
2022-06-15 14:24:50 -0400

    add subtitle

--- react-site/src/components/Contact.jsx
+++ react-site/src/components/Contact.jsx
@@ -9,10 +9,12 @@ export default function Contact(props) {
           <a className="contactLink" href="mailto:nealeptaylor@gmail.com">
             <i class="fa-solid fa-envelope"></i>
             <h5>Email</h5>
+            <h6>nealeptaylor@gmail.com</h6>
           </a>
           <a className="contactLink" href="https://www.linkedin.com/in/nealeptaylor/">
           <i class="fa-brands fa-linkedin-in"></i>
             <h5>LinkedIn</h5>
+            <h6>/in/nealeptaylor</h6>
           </a>
         </div>
       </div>
