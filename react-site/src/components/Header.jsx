aebbf861d6e30c5afcd0daee0a68cbca18779fe4
Neale Taylor <nealeptaylor@gmail.com>
2023-04-03 11:34:16 -0400

    modify header

--- react-site/src/components/Header.jsx
+++ react-site/src/components/Header.jsx
@@ -7,17 +7,29 @@ export default function Header(props) {
       linkClick={props.linkClick}
       setLinkClick={props.setLinkClick}
     />
+    <nav class="headNav">
+      <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
     <div className="wrapper">
-      <div className="titleCard">
-        <h1>
-          Neale Taylor
-        </h1>
-        <h2>
-          Full-Stack Web Developer
-        </h2>
-      </div>
-      <div className="resume">
-        <a href="./assets/NealeTaylorResume.pdf" target="_blank" className= "resumeClick">My Resume</a>
+      <div className="headerInfo">
+        <div className="titleCard">
+          <h1>
+            Neale Taylor
+          </h1>
+          <h2>
+            Full-Stack Web Developer
+          </h2>
+        </div>
+        <div className="resume">
+          <a href="./assets/NealeTaylorResume.pdf" target="_blank" className= "resumeClick">My Resume</a>
+        </div>
       </div>
     </div>
     </header>
