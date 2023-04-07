26c2abde4169235062fbe4eab945a078034aad1b
Neale Taylor <nealeptaylor@gmail.com>
2023-04-07 10:21:55 -0400

    add work info

--- react-site/src/components/Work.jsx
+++ react-site/src/components/Work.jsx
@@ -2,7 +2,14 @@ export default function Work() {
   return (
     <div className="Work">
       <div className="workCard">
-        
+        <img src="../../assets/FL2Flogo.png" alt="fl2f logo" />
+        <h5>Lighthouse Labs</h5>
+        <ul>
+          <li>Mainly using React, JS, Node, and Express, I created several new full-stack apps to my employers specifications.</li>
+          <li>Used Agile workflow to collaborate with colleagues and git for source control.</li>
+          <li>Analyzed code written by others in several languages to find and fix bugs or update code base.</li>
+          <li>Deployed apps using Google Cloud Repositories and Cloud run to deploy Docker containers onto the web.</li>
+        </ul>
       </div>
     </div>
   )
