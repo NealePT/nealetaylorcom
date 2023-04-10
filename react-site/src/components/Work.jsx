86b3f136cfa0119e4d634a7d8fa43eb7be5ec071
Neale Taylor <nealeptaylor@gmail.com>
2023-04-10 13:41:46 -0400

    add work history

--- react-site/src/components/Work.jsx
+++ react-site/src/components/Work.jsx
@@ -3,12 +3,37 @@ export default function Work() {
     <div className="Work">
       <div className="workCard">
         <img src="../../assets/FL2Flogo.png" alt="fl2f logo" />
-        <h5>Lighthouse Labs</h5>
+        <h5>Software Engineer</h5>
+        <h6>From Lab 2 Fulfillment</h6>
         <ul>
           <li>Mainly using React, JS, Node, and Express, I created several new full-stack apps to my employers specifications.</li>
           <li>Used Agile workflow to collaborate with colleagues and git for source control.</li>
           <li>Analyzed code written by others in several languages to find and fix bugs or update code base.</li>
           <li>Deployed apps using Google Cloud Repositories and Cloud run to deploy Docker containers onto the web.</li>
+        </ul>
+      </div>
+      <div className="workCard">
+        <h5>Web Developer</h5>
+        <h6>Freelance- Various Employers</h6>
+        <ul>
+          <li>Built, updated, and deployed several websites and web applications for various employers.</li>
+          <li>Used a variety of coding languages to deliver clean, functional code in a timely manner.</li>
+        </ul>
+      </div>
+      <div className="workCard">
+        <h5>Maintenance Supervisor</h5>
+        <h6>Grand Niagara</h6>
+        <ul>
+          <li>Managed a team of employees and led the completion of various projects.</li>
+          <li>Collaborated with management to increase worker efficiency in certain areas by up to 25%.</li>
+        </ul>
+      </div>
+      <div className="workCard">
+        <h5>Technical Support Advisor</h5>
+        <h6>Concentrix</h6>
+        <ul>
+          <li>Handled a variety of customer requests and dealt with any new issues as they arose.</li>
+          <li>Adapted and learned constantly for continued efficiency on calls.</li>
         </ul>
       </div>
     </div>
