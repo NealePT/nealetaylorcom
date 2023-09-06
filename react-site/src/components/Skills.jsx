1505b35d2cd616b11fdc4f0513bf6ff387efd4cf
Neale Taylor <nealeptaylor@gmail.com>
2023-09-06 15:01:25 -0400

    update skills section

--- react-site/src/components/Skills.jsx
+++ react-site/src/components/Skills.jsx
@@ -13,26 +13,27 @@ export default function Skills(props) {
             <h5 className={`skill-type ${props.skill==="BackEnd" && 'skill-type-selected'}`} onClick={(e) => {
               props.setSkill("BackEnd")
             }}>Back-End</h5>
-            <h5 className={`skill-type ${props.skill==="Testing" && 'skill-type-selected'}`} onClick={(e) => {
-              props.setSkill("Testing")
-            }}>Testing</h5>
+            <h5 className={`skill-type ${props.skill==="Frameworks" && 'skill-type-selected'}`} onClick={(e) => {
+              props.setSkill("Frameworks")
+            }}>Frameworks</h5>
+            <h5 className={`skill-type ${props.skill==="DevTools" && 'skill-type-selected'}`} onClick={(e) => {
+              props.setSkill("DevTools")
+            }}>Dev Tools</h5>
+            <h5 className={`skill-type ${props.skill==="SoftSkills" && 'skill-type-selected'}`} onClick={(e) => {
+              props.setSkill("SoftSkills")
+            }}>Soft Skills</h5>
           </aside>
           <article>
             {props.skill === "FrontEnd" && 
             <div>
               <ul>
                 <li>JavaScript</li>
-                <li>Ruby</li>
+                <li>React</li>
                 <li>HTML</li>
                 <li>CSS</li>
-                <li>React</li>
-                <li>EJS</li>
+                <li>SASS/SCSS</li>
+                <li>Ruby</li>
                 <li>Jquery</li>
-                <li>BootStrap</li>
-                <li>Sass/SCSS</li>
-                <li>MaterialUI</li>
-                <li>TailwindCSS</li>
-                <li>Storybook</li>
               </ul>
             </div>}
             {props.skill === "BackEnd" && 
@@ -40,20 +41,45 @@ export default function Skills(props) {
               <ul>
                 <li>Node.js</li>
                 <li>Express</li>
+                <li>SQL</li>
+                <li>Docker</li>
                 <li>Rails</li>
-                <li>Git</li>
-                <li>Ajax</li>
-                <li>PostgreSQL</li>
-                <li>MySQL</li>
               </ul>
             </div>}
-            {props.skill === "Testing" && 
+            {props.skill === "Frameworks" && 
             <div>
               <ul>
-                <li>Mocha</li>
-                <li>Chai</li>
-                <li>Cypress</li>
+                <li>jQuery</li>
+                <li>MySQL</li>
+                <li>PostgreSQL</li>
+                <li>MaterialUI</li>
+                <li>Bootstrap</li>
+                <li>Kubernetes</li>
+              </ul>
+            </div>}
+            {props.skill === "DevTools" && 
+            <div>
+              <ul>
+                <li>Git</li>
+                <li>Github</li>
+                <li>Google Cloud</li>
+                <li>Agile Development</li>
+                <li>React Testing Library</li>
+                <li>Mocha/Chai</li>
                 <li>Jest</li>
+                <li>Storybook</li>
+                <li>Cypress</li>
+              </ul>
+            </div>}
+            {props.skill === "SoftSkills" && 
+            <div>
+              <ul>
+                <li>Communication</li>
+                <li>Teamwork</li>
+                <li>Problem Solving</li>
+                <li>Critical Thinking</li>
+                <li>Leadership</li>
+                <li>Creativity</li>
               </ul>
             </div>}
           </article>
